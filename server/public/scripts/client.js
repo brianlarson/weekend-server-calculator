313827db9cd306f5260a9dce8c1da173dc78624e
Brian Larson <brian@brianlarson.com>
2024-11-07 23:05:26 -0600

    Finish getting sign buttons actinig like a radio btn group

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,32 +1,44 @@
 
+// Get our mathematical sign buttons
 let addBtn = document.getElementById("addBtn");
 let subtractBtn = document.getElementById("subtractBtn");
 let multiplyBtn = document.getElementById("multiplyBtn");
 let divideBtn = document.getElementById("divideBtn");
 
+// Make an array of the sign buttons
 let signButtons = [addBtn, subtractBtn, multiplyBtn, divideBtn];
 
+// Create function to handle selecting singular sign buttons
+// so the group behaves like a radio button group in a form
 function handleSignSelect(event) {
+
+  // Prevent default behavior of button elem
   event.preventDefault();
+
+  // Get button clicked and put its class list in an array
   let button = event.target;
   let buttonClasses = Array.from(button.classList);
-  let lastSelected;
-  for (const className of buttonClasses) {
-    switch (className) {
-      case "btn-primary":
-        button.classList.remove("btn-primary");
-        button.classList.add("btn-success");
-        button.setAttribute("data-selected", "true");
-        lastSelected = button.id;
-        break;
-      case "btn-success":
-        button.classList.remove("btn-success");
-        button.classList.add("btn-primary");
-        button.setAttribute("data-selected", "false");
-        break;
+
+  // First we reset ALL buttons in case one is already selected
+  resetSignButtons();
+
+  // After resetting all buttons we select the new one
+  button.classList.remove("btn-primary");
+  button.classList.add("btn-success");
+  button.setAttribute("data-selected", "true");
+
+}
+
+// Create function to reset sign buttons
+function resetSignButtons() {
+  for (const button of signButtons) {
+    // Get class list as an array of button
+    const classList = Array.from(button.classList);
+    // If any one is selected reset it
+    if (classList.includes("btn-success")) {
+      button.classList.remove("btn-success");
+      button.classList.add("btn-primary");
+      button.setAttribute("data-selected", "false");
     }
   }
-  // for (const button of signButtons) {
-  //   console.log(button.getAttribute("data-selected"));
-  // }
 }
