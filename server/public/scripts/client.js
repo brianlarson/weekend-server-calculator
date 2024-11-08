71c114e4f291242fec4ec472de5483dc392effa0
Brian Larson <brian@brianlarson.com>
2024-11-07 22:26:14 -0600

    First attempt at getting sign buttons to act like a radio button group

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,32 @@
-console.log('client.js is sourced!');
+
+let addBtn = document.getElementById("addBtn");
+let subtractBtn = document.getElementById("subtractBtn");
+let multiplyBtn = document.getElementById("multiplyBtn");
+let divideBtn = document.getElementById("divideBtn");
+
+let signButtons = [addBtn, subtractBtn, multiplyBtn, divideBtn];
+
+function handleSignSelect(event) {
+  event.preventDefault();
+  let button = event.target;
+  let buttonClasses = Array.from(button.classList);
+  let lastSelected;
+  for (const className of buttonClasses) {
+    switch (className) {
+      case "btn-primary":
+        button.classList.remove("btn-primary");
+        button.classList.add("btn-success");
+        button.setAttribute("data-selected", "true");
+        lastSelected = button.id;
+        break;
+      case "btn-success":
+        button.classList.remove("btn-success");
+        button.classList.add("btn-primary");
+        button.setAttribute("data-selected", "false");
+        break;
+    }
+  }
+  // for (const button of signButtons) {
+  //   console.log(button.getAttribute("data-selected"));
+  // }
+}
